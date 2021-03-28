e0f19bf80659b67a7520d06a622fa6a3078c5b86
Rishi Chopra <rqchopra@gmail.com>
2021-03-28 12:04:55 -0500

    Implement basic sum_to_n

--- systems/assembly/sum_to_n.asm
+++ systems/assembly/sum_to_n.asm
@@ -1,4 +1,11 @@
 section .text
 global sum_to_n
 sum_to_n:
+  mov rax, 0    ; set total = 0
+  mov rbx, 0    ; set i = 0
+loop:
+  add rax, rbx  ; add i to the total
+  add rbx, 1    ; increment i
+  cmp rbx, rdi  ; compare i to n (n is passed n via rdi)
+  jng loop      ; jump to the top of the loop if i <= n
 	ret
