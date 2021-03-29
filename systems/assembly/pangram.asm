9d5b7faf6e6757ce6212b2367c2b3f08a50ec465
Rishi Chopra <rqchopra@gmail.com>
2021-03-28 21:46:45 -0500

    Filter out punctuation with conditionals

--- systems/assembly/pangram.asm
+++ systems/assembly/pangram.asm
@@ -7,14 +7,20 @@ check:
   movzx rcx, byte [rdi] ; read the next char of input into r8
   cmp   rcx, 0          ; check if string is terminated
   je    done            ; jump to return if null byte
+  inc rdi               ; move to the next character in the string
 
-  mov r10, 1         ; a placeholder that will be shifted below
+  cmp rcx, 64        ; letters start at 65 ASCII
+  jng check          ; skip this char if not at least 65
+
   and rcx, 11111b    ; mask the low five bits of the char so 'a'=1, 'b'=2,...
+  cmp rcx, 27        ; we only care about 26 letters
+  jnl check          ; skip this char if it is not at most 26
+
   dec rcx            ; decrement rcx so 'a' shifts 0, 'b' shifts 1, ...
+  mov r10, 1         ; the placeholder that will be shifted
   shl r10, cl        ; shift left by cl (low 8 bits of rcx); 'h' gives 10000000
   or r8, r10         ; r8 is our "seen" array, so set the bit for that letter
 
-  inc rdi            ; move to the next character in the string
   cmp r8, 0x03ffffff ; check if the lowest 26 bits of the seen array are all 1
   jne check          ; jump and keep checking if not all 1s seen
   mov rax, 1         ; otherwise, set the return value to true and return
