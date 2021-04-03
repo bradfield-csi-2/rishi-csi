b5e61cc7fd3932d4f4be2a717c544daf8ac1ffbb
Rishi Chopra <rqchopra@gmail.com>
2021-04-02 22:57:32 -0500

    Implement array access solution

--- systems/assembly-2/matrix.asm
+++ systems/assembly-2/matrix.asm
@@ -6,4 +6,7 @@ index:
 	; rdx: cols
 	; rcx: rindex
 	; r8: cindex
+  imul rcx, rdx         ; offset of beginning of row
+  add r8, rcx           ; add num cols to the row offset
+  mov rax, [rdi + r8*4] ; get the value from memory, accounting for 4-byte ints
 	ret
