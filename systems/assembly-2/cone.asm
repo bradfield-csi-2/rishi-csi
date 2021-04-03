de9a9938881ca16696300f6dd899edacf4754c48
Rishi Chopra <rqchopra@gmail.com>
2021-04-03 15:56:05 -0500

    Implement volume function

--- systems/assembly-2/cone.asm
+++ systems/assembly-2/cone.asm
@@ -3,4 +3,12 @@ default rel
 section .text
 global volume
 volume:
+  mulss xmm0, xmm0     ; r^2
+  divss xmm1, [three]  ; h/3
+  mulss xmm0, xmm1     ; (r^2)h/3
+  mulss xmm0, [pi]     ; pi(r^2)h/3
  	ret
+
+section .rodata
+pi:    dd 3.14159265359
+three: dd 3.0
