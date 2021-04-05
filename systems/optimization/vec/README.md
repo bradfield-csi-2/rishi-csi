ac788fe788d84bd2b9e8a87e0f4eee2fac132a71
Rishi Chopra <rqchopra@gmail.com>
2021-04-05 13:05:22 -0500

    Skip bounds checking for vector elements

--- systems/optimization/vec/README.md
+++ systems/optimization/vec/README.md
@@ -5,3 +5,6 @@ Original:  .009s
 
 # Calculate length before looping
 Length outside loop: .006s
+
+# Remove bounds checking in get_vec_element
+No bounds checking: .002s
--- systems/optimization/vec/dotproduct.c
+++ systems/optimization/vec/dotproduct.c
@@ -2,13 +2,14 @@
 
 
 data_t dotproduct(vec_ptr u, vec_ptr v) {
-   data_t sum = 0, u_val, v_val;
+   data_t sum = 0;
    long length = vec_length(u); // we can assume both vectors are same length
 
-   for (long i = 0; i < length; i++) { // we can assume both vectors are same length
-        get_vec_element(u, i, &u_val);
-        get_vec_element(v, i, &v_val);
-        sum += u_val * v_val;
+   data_t *u_data = get_vec_start(u);
+   data_t *v_data = get_vec_start(v);
+
+   for (long i = 0; i < length; i++) {
+      sum += u_data[i] * v_data[i];
    }
    return sum;
 }
