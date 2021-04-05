a70b2bb0860b7356a1b40c39f6eac8835f30791b
Rishi Chopra <rqchopra@gmail.com>
2021-04-05 17:02:01 -0500

    Unroll the loop 3x3

--- systems/optimization/vec/dotproduct.c
+++ systems/optimization/vec/dotproduct.c
@@ -1,7 +1,27 @@
 #include "vec.h"
 
-// Unroll the loop
+// Unroll the loop 3x3
 data_t dotproduct(vec_ptr u, vec_ptr v) {
+   data_t sum1 = 0, sum2 = 0, sum3 = 0;
+   long i, length = vec_length(u); // we can assume both vectors are same length
+
+   data_t *u_data = get_vec_start(u);
+   data_t *v_data = get_vec_start(v);
+
+   for (i = 0; i < length; i+=3) {
+      sum1 += u_data[i] * v_data[i];
+      sum2 += u_data[i+1] * v_data[i+1];
+      sum3 += u_data[i+2] * v_data[i+2];
+   }
+
+   for (; i < length; i++) {
+      sum1 += u_data[i] * v_data[i];
+   }
+   return sum1 + sum2 + sum3;
+}
+
+// Unroll the loop 3x1
+data_t dotproduct5(vec_ptr u, vec_ptr v) {
    data_t sum = 0;
    long i, length = vec_length(u); // we can assume both vectors are same length
 
