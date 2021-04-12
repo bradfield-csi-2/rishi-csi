6d59febd980556fcf3cb264217441f00494bd185
Rishi Chopra <rqchopra@gmail.com>
2021-04-12 01:00:51 +0000

    Change loop order to improve locality

--- systems/memory-hierarchy/matrix-multiply.c
+++ systems/memory-hierarchy/matrix-multiply.c
@@ -7,6 +7,8 @@ There must be a better way!
 #include <stdio.h>
 #include <stdlib.h>
 
+#define TILES 32
+
 /*
   A naive implementation of matrix multiplication.
 
@@ -26,6 +28,12 @@ void matrix_multiply(double **C, double **A, double **B, int a_rows, int a_cols,
 
 void fast_matrix_multiply(double **c, double **a, double **b, int a_rows,
                           int a_cols, int b_cols) {
-  // TODO: write a faster implementation here!
-  return matrix_multiply(c, a, b, a_rows, a_cols, b_cols);
+  for (int k = 0; k < a_cols; k++) {
+    for (int i = 0; i < a_rows; i++) {
+      double r = a[i][k]; // stick row elem into a register
+      for (int j = 0; j < b_cols; j++) {
+        c[i][j] += r * b[k][j];
+      }
+    }
+  }
 }
