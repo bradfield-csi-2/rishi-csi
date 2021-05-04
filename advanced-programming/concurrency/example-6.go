0644b05443afe63b0ed57e958a74dc5361a7af44
Rishi Chopra <rqchopra@gmail.com>
2021-05-04 18:56:21 -0500

    Fix debugging example 6

--- advanced-programming/concurrency/example-6.go
+++ advanced-programming/concurrency/example-6.go
@@ -24,11 +24,23 @@ func (c *coordinator) logState() {
 	fmt.Printf("leader = %q\n", c.leader)
 }
 
-func (c *coordinator) setLeader(leader string, shouldLog bool) {
+func (c *coordinator) setLeader_orig(leader string, shouldLog bool) {
 	c.lock.Lock()
 	defer c.lock.Unlock()
 
 	c.leader = leader
+
+	if shouldLog {
+		c.logState()
+	}
+}
+
+// We were locking the entire method, including a call to logState which also
+// used a lock. If we only protect the critical section then we avoid deadlock
+func (c *coordinator) setLeader(leader string, shouldLog bool) {
+	c.lock.Lock()
+	c.leader = leader
+	c.lock.Unlock()
 
 	if shouldLog {
 		c.logState()
