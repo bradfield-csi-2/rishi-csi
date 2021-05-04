9cf051d86bdf7080d7523e2093696c5deacf9265
Rishi Chopra <rqchopra@gmail.com>
2021-05-04 18:22:16 -0500

    Fix debugging example 2

--- advanced-programming/concurrency/example-2.go
+++ advanced-programming/concurrency/example-2.go
@@ -6,7 +6,7 @@ import (
 
 const numTasks = 3
 
-func main() {
+func main_orig() {
 	var done chan struct{}
 	for i := 0; i < numTasks; i++ {
 		go func() {
@@ -20,6 +20,28 @@ func main() {
 	// Wait for tasks to complete
 	for i := 0; i < numTasks; i++ {
 		<-done
+	}
+	fmt.Printf("all %d tasks done!\n", numTasks)
+}
+
+// This is causing a deadlock because we're sending in the main goroutine and
+// so we will block before we can move forward in the loop
+func main() {
+	var done chan struct{}
+	for i := 0; i < numTasks; i++ {
+		go func() {
+			fmt.Println("running task...")
+
+			// Signal that task is done
+			done <- struct{}{}
+		}()
+	}
+
+	// Wait for tasks to complete
+	for i := 0; i < numTasks; i++ {
+		go func() {
+			<-done
+		}
 	}
 	fmt.Printf("all %d tasks done!\n", numTasks)
 }
