a187dae31826d68eb6fd384f35d5bbd2ae7dc86c
Rishi Chopra <rqchopra@gmail.com>
2021-05-04 08:43:47 -0500

    Fix debugging example 1

--- advanced-programming/concurrency/example-1.go
+++ advanced-programming/concurrency/example-1.go
@@ -5,11 +5,23 @@ import (
 	"time"
 )
 
-func main() {
+func main_orig() {
 	for i := 0; i < 10; i++ {
 		go func() {
 			fmt.Printf("launched goroutine %d\n", i)
 		}()
+	}
+	// Wait for goroutines to finish
+	time.Sleep(time.Second)
+}
+
+// The bug here is not passing the loop variable to the goroutine. Each one
+// will just see the last value, which is 10 in this case.
+func main() {
+	for i := 0; i < 10; i++ {
+		go func(i int) {
+			fmt.Printf("launched goroutine %d\n", i)
+		}(i)
 	}
 	// Wait for goroutines to finish
 	time.Sleep(time.Second)
