109a0cafe2b764126944af64a4ef575be05dde50
Rishi Chopra <rqchopra@gmail.com>
2021-05-04 18:29:51 -0500

    Fix debugging example 4

--- advanced-programming/concurrency/example-4.go
+++ advanced-programming/concurrency/example-4.go
@@ -4,8 +4,22 @@ import (
 	"fmt"
 )
 
-func main() {
+func main_orig() {
 	done := make(chan struct{}, 1)
+	go func() {
+		fmt.Println("performing initialization...")
+		<-done
+	}()
+
+	done <- struct{}{}
+	fmt.Println("initialization done, continuing with rest of program")
+}
+
+// The initialization can't happen on a buffered channel because the receive
+// won't block the main goroutine. Instead, we have to make it an unbuffered
+// channel so that we wait for intialization
+func main() {
+	done := make(chan struct{})
 	go func() {
 		fmt.Println("performing initialization...")
 		<-done
