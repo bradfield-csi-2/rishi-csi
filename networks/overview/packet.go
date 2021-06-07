5a88d22e16e2143760fa7033a4318190da84552b
Rishi Chopra <rqchopra@gmail.com>
2021-06-07 08:40:10 -0500

    Iterate over all packets

--- networks/overview/packet.go
+++ networks/overview/packet.go
@@ -25,22 +25,52 @@ type PcapPacketHeader struct {
 	UntruncatedLength  uint32
 }
 
+type EthernetFrameHeader struct {
+	MACDst    [6]byte
+	MACSrc    [6]byte
+	Ethertype uint16
+}
+
+type Packet struct {
+	EthernetFrameHeader
+	Payload       [64]byte
+	FrameCheckSeq [4]byte
+}
+
 func main() {
 	fmt.Printf("Parsing the packet capture...\n\n")
 	f, err := os.Open("net.cap")
+	defer f.Close()
 	if err != nil {
 		fmt.Errorf("Could not read packet capture file: %v", err)
 	}
+	stat, _ := f.Stat()
+	capturedBytes := stat.Size()
+	var offset int64
 
 	fileHeader := parseFileHeader(f)
 	fmt.Printf("Pcap File Header:\n%+v\n", fileHeader)
 
-	headerSize := int64(unsafe.Sizeof(*fileHeader))
+	offset += int64(unsafe.Sizeof(*fileHeader))
 	snapshotLen := int64(fileHeader.SnapshotLen)
 
-	packetReader := io.NewSectionReader(f, headerSize, snapshotLen)
-	packetHeader := parsePacketHeader(packetReader)
-	fmt.Printf("Pcap Packet Header:\n%+v\n", packetHeader)
+	for offset < capturedBytes {
+		packetHeaderReader := io.NewSectionReader(f, offset, snapshotLen)
+		packetHeader := parsePacketHeader(packetHeaderReader)
+		offset += int64(unsafe.Sizeof(*packetHeader))
+		fmt.Printf("Pcap Packet Header:\n%+v\n", packetHeader)
+
+		packetReader := io.NewSectionReader(f, offset, int64(packetHeader.Length))
+		packet := parsePacket(packetReader)
+		offset += int64(packetHeader.Length)
+		fmt.Printf("Packet:\n%+v\n", packet)
+	}
+}
+
+func parsePacket(f io.Reader) *EthernetFrameHeader {
+	packet := new(EthernetFrameHeader)
+	binary.Read(f, binary.BigEndian, packet)
+	return packet
 }
 
 func parseFileHeader(f io.Reader) *PcapFileHeader {
