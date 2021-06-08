66a0fb834274dc96ad8010cd5ca730988c855b33
Rishi Chopra <rqchopra@gmail.com>
2021-06-07 21:54:47 -0500

    Combine TCP segments and parse the final HTTP

--- networks/overview/packet.go
+++ networks/overview/packet.go
@@ -4,7 +4,10 @@ import (
 	"encoding/binary"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
+	"regexp"
+	"sort"
 	"unsafe"
 )
 
@@ -55,6 +58,17 @@ type TCPSegmentHeader struct {
 	UrgentPtr                 uint16
 }
 
+type ImagePacket struct {
+	SeqNum uint32
+	Data   []byte
+}
+
+type BySeqNum []ImagePacket
+
+func (a BySeqNum) Len() int           { return len(a) }
+func (a BySeqNum) Less(i, j int) bool { return a[i].SeqNum < a[j].SeqNum }
+func (a BySeqNum) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+
 func main() {
 	fmt.Printf("Parsing the packet capture...\n\n")
 	f, err := os.Open("net.cap")
@@ -73,41 +87,77 @@ func main() {
 	snapshotLen := int64(fileHeader.SnapshotLen)
 
 	var packetCount int
+	var i int
+	imagePackets := make([]ImagePacket, 100)
 	for offset < capturedBytes {
 		pcapHeaderReader := io.NewSectionReader(f, offset, snapshotLen)
 		pcapHeader := parsePcapHeader(pcapHeaderReader)
 		offset += int64(unsafe.Sizeof(*pcapHeader))
-		fmt.Printf("Pcap Packet Header:\n%+v\n", pcapHeader)
+		//fmt.Printf("Pcap Packet Header:\n%+v\n", pcapHeader)
 
 		ethReader := io.NewSectionReader(f, offset, int64(pcapHeader.Length))
 		ethHeader := parsePacket(ethReader)
 		ethHeaderLen := int64(unsafe.Sizeof(*ethHeader))
 		offset += ethHeaderLen
-		fmt.Printf("Ethernet Header:\n%+v\n", ethHeader)
+		//fmt.Printf("Ethernet Header:\n%+v\n", ethHeader)
 
 		ipHeaderReader := io.NewSectionReader(f, offset, int64(pcapHeader.Length))
 		ipHeader := parseIPHeader(ipHeaderReader)
 		ipHeaderLen := int64((ipHeader.Version_IHL<<4)>>4) * 4
 		offset += ipHeaderLen
-		fmt.Printf("IP Datagram Header:\n%+v\nLength: %d\n", ipHeader, ipHeaderLen)
+		//fmt.Printf("IP Datagram Header:\n%+v\nLength: %d\n", ipHeader, ipHeaderLen)
 
 		tcpSegmentReader := io.NewSectionReader(f, offset, int64(pcapHeader.Length))
 		tcpSegmentHeader := parseTCPHeader(tcpSegmentReader)
 		dataOffset := int64(((tcpSegmentHeader.DataOffset_Reserved_Flags[0]) >> 4) * 4)
-		fmt.Printf("TCP Segment Header:\n%+v\nData Offset: %d\n", tcpSegmentHeader, dataOffset)
+		//fmt.Printf("TCP Segment Header:\n%+v\nData Offset: %d\n", tcpSegmentHeader, dataOffset)
 
 		tcpSegmentDataReader := io.NewSectionReader(f, offset+dataOffset, int64(pcapHeader.Length))
 		tcpData := make([]byte, int64(ipHeader.Length)-ipHeaderLen-dataOffset)
 		tcpSegmentDataReader.Read(tcpData)
-		fmt.Printf("%s\n", tcpData)
+		//fmt.Printf("%s\n", tcpData)
+
+		if tcpSegmentHeader.SrcPort == 80 {
+			imagePackets[i] = ImagePacket{
+				SeqNum: tcpSegmentHeader.SeqNum,
+				Data:   tcpData,
+			}
+			i++
+		}
 
 		offset += int64(pcapHeader.Length) - ethHeaderLen - ipHeaderLen
-
 		packetCount++
 		fmt.Println("======================================")
 	}
 
 	fmt.Printf("Total Packets: %d\n", packetCount)
+	combined := combineTcpResponse(imagePackets)
+
+	httpHeader, imageData := parseHTTPResponse(combined)
+	fmt.Printf("%s\n", httpHeader)
+	ioutil.WriteFile("img.jpg", imageData, 0644)
+}
+
+func parseHTTPResponse(resp []byte) (header string, image []byte) {
+	respStr := string(resp)
+	emptyLine := regexp.MustCompile(`\r\n\r\n`)
+	parts := emptyLine.Split(respStr, 2)
+	header = parts[0]
+	image = []byte(parts[1])
+	return
+}
+
+func combineTcpResponse(imagePackets []ImagePacket) []byte {
+	sort.Sort(BySeqNum(imagePackets))
+	resp := make([]byte, 1000)
+	for i, pkt := range imagePackets {
+		if i > 0 && (imagePackets[i-1].SeqNum == pkt.SeqNum) {
+			continue
+		}
+		resp = append(resp, pkt.Data...)
+	}
+
+	return resp
 }
 
 func parseTCPHeader(f io.Reader) *TCPSegmentHeader {
