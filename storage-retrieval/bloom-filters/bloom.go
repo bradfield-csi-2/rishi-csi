746ea5491bb635e04b39dc2214316823be897760
Rishi Chopra <rqchopra@gmail.com>
2021-07-16 13:10:39 -0700

    [bloom-filters] Implement basic filter with k=2
    The number of hash functions is hard-coded for now, as I still need to
    look into how to make n different hash functions. Also performance is
    not great so need to look into that.

--- storage-retrieval/bloom-filters/bloom.go
+++ storage-retrieval/bloom-filters/bloom.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"encoding/binary"
+	"hash"
+	"hash/fnv"
+	"math/bits"
 )
 
 type bloomFilter interface {
@@ -15,25 +18,55 @@ type bloomFilter interface {
 	memoryUsage() int
 }
 
-type trivialBloomFilter struct {
-	data []uint64
+type BloomFilter struct {
+	data    []byte
+	size    uint64
+	k       int
+	hashFns []hash.Hash64
 }
 
-func newTrivialBloomFilter() *trivialBloomFilter {
-	return &trivialBloomFilter{
-		data: make([]uint64, 1000),
+// |/usr/share/dict/words| = 235886
+func newBloomFilter() *BloomFilter {
+	size := uint64(100000)
+	return &BloomFilter{
+		data:    make([]byte, size),
+		size:    size,
+		k:       2,
+		hashFns: []hash.Hash64{fnv.New64(), fnv.New64a()},
 	}
 }
 
-func (b *trivialBloomFilter) add(item string) {
-	// Do nothing
+func (b *BloomFilter) add(item string) {
+	for _, h := range b.hashFns {
+		h.Write([]byte(item))
+		sum := h.Sum64()
+		// Get overall bit position in array of size*8 bits
+		i := sum % (b.size * 8)
+		// Then split into array index and remainder
+		quo, rem := bits.Div64(0, i, 8)
+		// Remainder is the bit offset into that byte
+		var offset byte = 1 << rem
+		// Turn on that bit in the byte
+		b.data[quo] |= offset
+		h.Reset()
+	}
 }
 
-func (b *trivialBloomFilter) maybeContains(item string) bool {
-	// Technically, any item "might" be in the set
+func (b *BloomFilter) maybeContains(item string) bool {
+	for _, h := range b.hashFns {
+		h.Write([]byte(item))
+		sum := h.Sum64()
+		h.Reset()
+		i := sum % (b.size * 8)
+		quo, rem := bits.Div64(0, i, 8)
+		var offset byte = 1 << rem
+		if b.data[quo]&(offset) == 0 {
+			return false
+		}
+	}
 	return true
 }
 
-func (b *trivialBloomFilter) memoryUsage() int {
+func (b *BloomFilter) memoryUsage() int {
 	return binary.Size(b.data)
 }
