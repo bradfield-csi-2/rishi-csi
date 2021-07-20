90350ced4e39b3ef20c6569cb0439ae33f340b9f
Rishi Chopra <rqchopra@gmail.com>
2021-07-20 14:13:12 -0700

    [lsm-trees] Design a simple format for point reads
    When the file is opened, the index section is read into memory (with the
    help of the two-byte header section to find where to stop reading the
    index) and stored as a map of the string key to the offset values. A
    point read looks up the key in the map and then seeks to that offset in
    the file.
    
    The file format has three sections.
    
    Section 1: Header (2 bytes)
    ---------------------------
    A uint16 representing the length of the Index section
    
    Section 2: Index (len(index) bytes)
    -----------------------------------
    These are index entries packed one after the other. Each entry has a
    six-byte header followed by the key itself. The six-byte header consists
    of three uint16s: the value's offset into the data section as well as the
    length of the key and value strings.
    
    Section 3: Data (len(values) bytes)
    -----------------------------------
    These are simply the values themselves packed one after the other.

--- storage-retrieval/lsm-trees/table.go
+++ storage-retrieval/lsm-trees/table.go
@@ -1,11 +1,46 @@
 package table
 
+import (
+	"bytes"
+	"encoding/binary"
+	"io/ioutil"
+	"os"
+)
+
 type Item struct {
 	Key, Value string
 }
 
 // Given a sorted list of key/value pairs, write them out according to the format you designed.
 func Build(path string, sortedItems []Item) error {
+	data := new(bytes.Buffer)
+	index := new(bytes.Buffer)
+	var dataOffset uint16 = 0
+	var indexLen uint16 = 0
+	for _, item := range sortedItems {
+		keyLen := uint16(len(item.Key))
+		valLen := uint16(len(item.Value))
+
+		// Write an index entry, containing the value's offset into the data
+		// portion of the file, the length of the value, length of the key, and the
+		// key itself
+		entry := &IndexEntry{dataOffset, valLen, keyLen}
+		binary.Write(index, binary.BigEndian, entry)
+		index.WriteString(item.Key)
+
+		// Write the value in data portion of file
+		data.WriteString(item.Value)
+
+		indexLen += (keyLen + 6)
+		dataOffset += valLen
+	}
+	file := new(bytes.Buffer)
+	// Place the length of the index as the first two bytes in the file, then
+	// write the index and data
+	binary.Write(file, binary.BigEndian, indexLen)
+	file.Write(index.Bytes())
+	file.Write(data.Bytes())
+	ioutil.WriteFile(path, file.Bytes(), 0644)
 	return nil
 }
 
@@ -15,17 +50,58 @@ func Build(path string, sortedItems []Item) error {
 // Although a Table shouldn't keep all the key/value data in memory, it should contain
 // some metadata to help with efficient access (e.g. size, index, optional Bloom filter).
 type Table struct {
-	// TODO
+	indexLen uint16
+	index    map[string]IndexEntry
+	file     *os.File
+}
+
+type IndexEntry struct {
+	Offset uint16
+	ValLen uint16
+	KeyLen uint16
 }
 
 // Prepares a Table for efficient access. This will likely involve reading some metadata
 // in order to populate the fields of the Table struct.
 func LoadTable(path string) (*Table, error) {
-	return nil, nil
+	f, _ := os.Open(path)
+	d := make([]byte, 2)
+	f.Read(d)
+	indexLen := binary.BigEndian.Uint16(d)
+
+	indexBytes := make([]byte, indexLen)
+	f.ReadAt(indexBytes, 2)
+	index := make(map[string]IndexEntry)
+
+	var i uint16 = 0
+	for i < indexLen {
+		entry := new(IndexEntry)
+		r := bytes.NewReader(indexBytes[i : i+6])
+		binary.Read(r, binary.BigEndian, entry)
+		i += 6
+
+		key := string(indexBytes[i : i+entry.KeyLen])
+		index[key] = *entry
+		i += entry.KeyLen
+	}
+
+	t := &Table{
+		file:     f,
+		indexLen: indexLen,
+		index:    index,
+	}
+
+	return t, nil
 }
 
 func (t *Table) Get(key string) (string, bool, error) {
-	return "", false, nil
+	entry, ok := t.index[key]
+	if !ok {
+		return "", false, nil
+	}
+	value := make([]byte, entry.ValLen)
+	t.file.ReadAt(value, int64(entry.Offset+t.indexLen+2))
+	return string(value), true, nil
 }
 
 func (t *Table) RangeScan(startKey, endKey string) (Iterator, error) {
