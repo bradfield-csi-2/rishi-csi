b49b722f6893ce12688f76e9279dc617946a691a
Rishi Chopra <rqchopra@gmail.com>
2021-08-01 11:28:46 -0700

    [roaring-bitmaps] Implement basic Get and Set

--- storage-retrieval/roaring-bitmaps/uncompressed.go
+++ storage-retrieval/roaring-bitmaps/uncompressed.go
@@ -7,15 +7,17 @@ type uncompressedBitmap struct {
 }
 
 func newUncompressedBitmap() *uncompressedBitmap {
-	return &uncompressedBitmap{}
+	return &uncompressedBitmap{data: make([]uint64, 100000)}
 }
 
 func (b *uncompressedBitmap) Get(x uint32) bool {
-	return false
+	index, offset := x/wordSize, x%wordSize
+	return b.data[index]&(1<<offset) != 0
 }
 
 func (b *uncompressedBitmap) Set(x uint32) {
-	// Do nothing
+	index, offset := x/wordSize, x%wordSize
+	b.data[index] |= 1 << offset
 }
 
 func (b *uncompressedBitmap) Union(other *uncompressedBitmap) *uncompressedBitmap {
