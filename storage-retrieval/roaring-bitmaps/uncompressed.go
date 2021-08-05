052b79d8f041bd22b33cf0e9c143d13bb3baf580
Rishi Chopra <rqchopra@gmail.com>
2021-08-05 11:01:14 -0700

    [roaring-bitmaps] Grow bitmaps dynamically

--- storage-retrieval/roaring-bitmaps/uncompressed.go
+++ storage-retrieval/roaring-bitmaps/uncompressed.go
@@ -7,23 +7,48 @@ type uncompressedBitmap struct {
 }
 
 func newUncompressedBitmap() *uncompressedBitmap {
-	return &uncompressedBitmap{data: make([]uint64, 100000)}
+	return &uncompressedBitmap{data: make([]uint64, 1)}
 }
 
 func (b *uncompressedBitmap) Get(x uint32) bool {
 	index, offset := x/wordSize, x%wordSize
+	// Bitmap is too small to contain this value
+	if index >= uint32(len(b.data)) {
+		return false
+	}
 	return b.data[index]&(1<<offset) != 0
 }
 
 func (b *uncompressedBitmap) Set(x uint32) {
 	index, offset := x/wordSize, x%wordSize
+
+	// If our bitmap isn't big enough, grow it
+	overshoot := int(index) - int(len(b.data)) + 1
+	if overshoot > 0 {
+		extra := make([]uint64, overshoot)
+		b.data = append(b.data, extra...)
+	}
+
 	b.data[index] |= 1 << offset
 }
 
 func (b *uncompressedBitmap) Union(other *uncompressedBitmap) *uncompressedBitmap {
-	data := make([]uint64, len(b.data))
+	// Set the new bitmap length to be the longest of the two
+	var longest int
+	if len(b.data) > len(other.data) {
+		longest = len(b.data)
+	} else {
+		longest = len(other.data)
+	}
+	data := make([]uint64, longest)
 	for i, _ := range data {
-		data[i] = b.data[i] | other.data[i]
+		if i >= len(b.data) {
+			data[i] = other.data[i]
+		} else if i >= len(other.data) {
+			data[i] = b.data[i]
+		} else {
+			data[i] = b.data[i] | other.data[i]
+		}
 	}
 	return &uncompressedBitmap{
 		data: data,
@@ -31,7 +56,14 @@ func (b *uncompressedBitmap) Union(other *uncompressedBitmap) *uncompressedBitma
 }
 
 func (b *uncompressedBitmap) Intersect(other *uncompressedBitmap) *uncompressedBitmap {
-	data := make([]uint64, len(b.data))
+	// Set the new bitmap length to be the shortest of the two
+	var shortest int
+	if len(b.data) < len(other.data) {
+		shortest = len(b.data)
+	} else {
+		shortest = len(other.data)
+	}
+	data := make([]uint64, shortest)
 	for i, _ := range data {
 		data[i] = b.data[i] & other.data[i]
 	}
