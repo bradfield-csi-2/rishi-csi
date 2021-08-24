3ac2a1fdeb7e1eb3c80f1f6f7d0eefa8acb763d6
Rishi Chopra <rqchopra@gmail.com>
2021-08-24 13:47:22 -0700

    [process-execution] Log all signals using sigaction

--- oses/process-execution/log_signals.c
+++ oses/process-execution/log_signals.c
@@ -1,6 +1,31 @@
+#include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
-int main(int argc, char *argv[]) {
-	printf("Log Signals\n");
-	return 0;
+static void handler(int signum) {
+  printf("Received Signal %d: %s!\n", signum, strsignal(signum));
+}
+
+int main() {
+  struct sigaction sa;
+
+  sa.sa_handler = handler;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = SA_RESTART; // Restart functions if interrupted by handler
+
+  // Set handler for all signal numbers between 1 and SIGSYS (31)
+  for (int i = 1; i <= SIGSYS; i++) {
+    // These signals can be neither caught nor ignored
+    if (i == SIGKILL || i == SIGSTOP) continue;
+
+    if (sigaction(i, &sa, NULL) == -1) {
+      printf("Error setting signal: %d\n", i);
+      exit(1);
+    }
+  }
+
+  printf("PID: %d\nWaiting for a signal...\n", getpid());
+  while(1);
 }
