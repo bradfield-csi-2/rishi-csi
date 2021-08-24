e9cb8cfcc33c7c55f00a40bc8a68d5a5372da05a
Rishi Chopra <rqchopra@gmail.com>
2021-08-24 15:23:58 -0700

    [process-execution] Use waitpid to log how the child terminated
    The program will sleep for a bit allowing a user to send a signal,
    but will otherwise terminated normally.

--- oses/process-execution/child.c
+++ oses/process-execution/child.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <unistd.h>
+
+#define SLEEP_SEC 5
 
 int main(int argc, char *argv[]) {
-	printf("Child Process Termination\n");
+  int status;
+
+  pid_t pid;
+  pid = fork();
+
+  if (pid == 0) {
+    printf("Child PID: %d\nWaiting for a signal for %d seconds.\n", getpid(), SLEEP_SEC);
+    sleep(SLEEP_SEC);
+  }
+
+  if ((pid = waitpid(pid, &status, 0)) > 0) {
+    if (WIFEXITED(status)) {
+      printf("Exited normally with status %d.\n", WEXITSTATUS(status));
+    } else if (WIFSIGNALED(status)) {
+      printf("Terminated via signal %d: %s.\n", WTERMSIG(status), strsignal(status));
+    }
+  }
+
   return 0;
 }
