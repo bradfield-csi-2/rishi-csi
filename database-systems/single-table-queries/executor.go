11bbeb1fae8fa4adfe8b11b0a6724318061bc4d6
Rishi Chopra <rqchopra@gmail.com>
2021-09-24 14:49:49 -0500

    [sing-tbl-qs] Read MovieLens data

--- database-systems/single-table-queries/executor.go
+++ database-systems/single-table-queries/executor.go
@@ -1,13 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+)
+
+type movie struct {
+	movieId string
+	title   string
+	genres  string
+}
 
 type Node interface {
-	Next() []string
+	Next() movie
 }
 
 type SeqScanNode struct {
-	data   [][]string
+	data   []*movie
 	nRows  int
 	cursor int
 	child  *Node
@@ -20,11 +30,23 @@ type LimitNode struct {
 }
 
 func newSeqScanNode() *SeqScanNode {
-	data := [][]string{
-		{"a1", "a2", "a3"},
-		{"b1", "b2", "b3"},
-		{"c1", "c2", "c3"},
+	data := make([]*movie, 0)
+	f, err := os.Open("data/movies.csv")
+	if err != nil {
+		fmt.Printf("Could not open movies file.")
+		return nil
 	}
+	r := csv.NewReader(f)
+	r.Read() // Skip header
+	movies, err := r.ReadAll()
+	if err != nil {
+		fmt.Printf("Could not read movies file.")
+		return nil
+	}
+	for _, m := range movies {
+		data = append(data, &movie{m[0], m[1], m[2]})
+	}
+
 	return &SeqScanNode{
 		data:   data,
 		nRows:  len(data),
@@ -37,16 +59,15 @@ func newLimitNode(limit int, child *SeqScanNode) *LimitNode {
 	return &LimitNode{limit: limit, cursor: 0, child: child}
 }
 
-func (l *LimitNode) Next() []string {
+func (l *LimitNode) Next() *movie {
 	if l.cursor < l.limit {
-		//fmt.Printf("c: %+v\n", c)
 		l.cursor++
 		return l.child.Next()
 	}
 	return nil
 }
 
-func (s *SeqScanNode) Next() []string {
+func (s *SeqScanNode) Next() *movie {
 	if s.cursor >= s.nRows {
 		return nil
 	}
@@ -57,9 +78,9 @@ func (s *SeqScanNode) Next() []string {
 }
 
 func main() {
-	fmt.Println("Executing: SELECT * FROM test LIMIT 2")
+	fmt.Println("Executing: SELECT * FROM test LIMIT 5")
 	s := newSeqScanNode()
-	l := newLimitNode(2, s)
+	l := newLimitNode(5, s)
 	for row := l.Next(); row != nil; row = l.Next() {
 		fmt.Printf("%+v\n", row)
 	}
