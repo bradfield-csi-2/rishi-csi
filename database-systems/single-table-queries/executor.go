e3b8174a6c759dce2e40d254b7d73ce739fec6e2
Rishi Chopra <rqchopra@gmail.com>
2021-09-24 14:24:03 -0500

    [sing-tbl-qs] Add basic Limit and SeqScan nodes
    Still need to set up proper interfaces where the Limit node does not
    necessarily have to take only a SeqScan node as a child.

--- database-systems/single-table-queries/executor.go
+++ database-systems/single-table-queries/executor.go
@@ -2,6 +2,65 @@ package main
 
 import "fmt"
 
+type Node interface {
+	Next() []string
+}
+
+type SeqScanNode struct {
+	data   [][]string
+	nRows  int
+	cursor int
+	child  *Node
+}
+
+type LimitNode struct {
+	limit  int
+	cursor int
+	child  *SeqScanNode
+}
+
+func newSeqScanNode() *SeqScanNode {
+	data := [][]string{
+		{"a1", "a2", "a3"},
+		{"b1", "b2", "b3"},
+		{"c1", "c2", "c3"},
+	}
+	return &SeqScanNode{
+		data:   data,
+		nRows:  len(data),
+		cursor: 0,
+		child:  nil,
+	}
+}
+
+func newLimitNode(limit int, child *SeqScanNode) *LimitNode {
+	return &LimitNode{limit: limit, cursor: 0, child: child}
+}
+
+func (l *LimitNode) Next() []string {
+	if l.cursor < l.limit {
+		//fmt.Printf("c: %+v\n", c)
+		l.cursor++
+		return l.child.Next()
+	}
+	return nil
+}
+
+func (s *SeqScanNode) Next() []string {
+	if s.cursor >= s.nRows {
+		return nil
+	}
+
+	row := s.data[s.cursor]
+	s.cursor++
+	return row
+}
+
 func main() {
-	fmt.Println("I am a query executor.")
+	fmt.Println("Executing: SELECT * FROM test LIMIT 2")
+	s := newSeqScanNode()
+	l := newLimitNode(2, s)
+	for row := l.Next(); row != nil; row = l.Next() {
+		fmt.Printf("%+v\n", row)
+	}
 }
