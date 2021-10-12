504b5047c606d4c7bb7d97067bfba0bf3e9d2526
Rishi Chopra <rqchopra@gmail.com>
2021-10-12 13:49:21 -0500

    [indexes] Start an index scanner node

--- database-systems/indexes/main.go
+++ database-systems/indexes/main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+	"bytes"
 	"encoding/csv"
+	"fmt"
 	"log"
 	"os"
+	"sort"
 )
+
+type RecordsByTitle [][]string
+
+func (r RecordsByTitle) Len() int {
+	return len(r)
+}
+
+func (r RecordsByTitle) Less(i, j int) bool {
+	return r[i][1] < r[j][1]
+}
+
+func (r RecordsByTitle) Swap(i, j int) {
+	r[i], r[j] = r[j], r[i]
+}
 
 func main() {
 	// Read the movies CSV
@@ -30,4 +47,22 @@ func main() {
 	if err := writer.Close(); err != nil {
 		log.Fatalf("error closing writer: %v", err)
 	}
+	indexScanner := NewIndexScanner(bytes.NewBuffer(nil), buildIndex(records), "")
+
+	for indexScanner.Next() {
+		fmt.Printf("%v", indexScanner.Execute())
+	}
+}
+
+func buildIndex(records [][]string) []Tuple {
+	tuples := make([]Tuple, len(records))
+	sort.Sort(RecordsByTitle(records))
+	for i, record := range records {
+		if i == 0 {
+			continue // Skip header
+		}
+		tuples[i] = newTuple("id", record[0], "title", record[1], "genres", record[2])
+	}
+
+	return tuples
 }
