8fd23fe9d2d1d61614848671b32cf93ae75a8d34
Rishi Chopra <rqchopra@gmail.com>
2021-10-23 23:32:34 -0500

    [distsys-intro] Parse input and store in memory
    As of now the only operations supported are `get` and `set` (case
    insensitive). There is simply a global `map[string]string` that is
    updated.
    
    There is minimal valid input checking, mainly that the first word is
    either get or set and that set operations are followed by 'key=value'.
    These are then placed in a `Command` struct that is then read by the
    function executing the command.

--- distsys/introduction/main.go
+++ distsys/introduction/main.go
@@ -9,6 +9,21 @@ import (
 	"syscall"
 )
 
+type Op int
+
+type Command struct {
+	op    Op
+	key   string
+	value string
+}
+
+const (
+	Get Op = iota
+	Set
+)
+
+var store map[string]string
+
 func HandleInt() {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
@@ -19,8 +34,52 @@ func HandleInt() {
 	}()
 }
 
+func ParseCommand(cmd string) (*Command, error) {
+	cmd = strings.TrimSpace(cmd)
+	i := strings.Index(cmd, " ")
+	if i < 0 {
+		return nil, fmt.Errorf("Invalid command: not enough arguments")
+	}
+
+	op := strings.ToLower(cmd[0:i])
+	args := cmd[i+1:]
+	command := &Command{}
+	if op == "get" {
+		command.op = Get
+		command.key = args
+	} else if op == "set" {
+		command.op = Set
+		parts := strings.Split(args, "=")
+		if len(parts) != 2 {
+			return nil, fmt.Errorf("Invalid command: must provide 'key=value' for set")
+		}
+		command.key = parts[0]
+		command.value = parts[1]
+	} else {
+		return nil, fmt.Errorf("Invalid command: unknown operation")
+	}
+	return command, nil
+}
+
+func ExecuteCommand(cmd *Command) (string, error) {
+	switch op := cmd.op; op {
+	case Get:
+		if val, ok := store[cmd.key]; ok {
+			return val, nil
+		} else {
+			return "", fmt.Errorf("Key '%s' not found", cmd.key)
+		}
+	case Set:
+		store[cmd.key] = cmd.value
+		return fmt.Sprintf("Set key '%s' to '%s'", cmd.key, cmd.value), nil
+	default:
+		return "", fmt.Errorf("Invalid Operation: %v", op)
+	}
+}
+
 func main() {
 	reader := bufio.NewReader(os.Stdin)
+	store = make(map[string]string)
 	HandleInt()
 
 	for {
@@ -30,7 +89,16 @@ func main() {
 			fmt.Println("Error reading input")
 			continue
 		}
-		line = strings.TrimSpace(line)
-		fmt.Println(line)
+		cmd, err := ParseCommand(line)
+		if err != nil {
+			fmt.Println(err.Error())
+			continue
+		}
+		result, err := ExecuteCommand(cmd)
+		if err != nil {
+			fmt.Println(err.Error())
+			continue
+		}
+		fmt.Println(result)
 	}
 }
